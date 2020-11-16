8026725e6b7a30d30d998cdd8daba90bb24906e1
sh05 <shogonakamoto0107@gmail.com>
2020-11-16 20:16:18 +0900

    wip

--- texsrc/tex_compile.sh
+++ texsrc/tex_compile.sh
@@ -1,7 +1,8 @@
 #!/bin/ash
 
-rm *.aux *.nav *.out *.snm *.toc *.dvi *.log
+rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log
 uplatex -output-directory=/workdir /workdir/main.tex
 dvipdfmx -o /workdir/main.pdf /workdir/main.dvi
 uplatex -output-directory=/workdir /workdir/main.tex
 dvipdfmx -o /workdir/main.pdf /workdir/main.dvi
+
