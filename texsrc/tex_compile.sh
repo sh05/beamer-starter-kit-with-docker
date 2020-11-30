d0b905b05dbc58c51f5b907798c56cb19d8efcaf
sh05 <shogonakamoto0107@gmail.com>
2020-12-01 08:43:41 +0900

    first draft

--- texsrc/tex_compile.sh
+++ texsrc/tex_compile.sh
@@ -1,8 +1,8 @@
 #!/bin/ash
 cd /workdir
-rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
 uplatex main.tex
 pbibtex main
 uplatex main.tex
 uplatex main.tex
 dvipdfmx main.dvi
+rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
--- texsrc/tex_compile_with_handout.sh
+++ texsrc/tex_compile_with_handout.sh
@@ -1,19 +1,19 @@
 #!/bin/ash
 cd /workdir
 
-rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
 uplatex main.tex
 pbibtex main
 uplatex main.tex
 uplatex main.tex
 dvipdfmx main.dvi
+rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
 
 cp /workdir/main.tex /workdir/handout.tex
 sed -i 's/]{beamer}/, handout]{beamer}/' /workdir/handout.tex
 
-rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
 uplatex handout.tex
 pbibtex handout
 uplatex handout.tex
 uplatex handout.tex
 dvipdfmx handout.dvi
+rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
