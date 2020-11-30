9eccc9d75ea3a83586429706ccbc9a5492d67153
sh05 <shogonakamoto0107@gmail.com>
2020-12-01 04:24:47 +0900

    add rm bbl and blg

--- texsrc/tex_compile.sh
+++ texsrc/tex_compile.sh
@@ -1,7 +1,6 @@
 #!/bin/ash
 cd /workdir
-
-rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log
+rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
 uplatex main.tex
 pbibtex main
 uplatex main.tex
--- texsrc/tex_compile_with_handout.sh
+++ texsrc/tex_compile_with_handout.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 cd /workdir
 
-rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log
+rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
 uplatex main.tex
 pbibtex main
 uplatex main.tex
@@ -11,7 +11,7 @@ dvipdfmx main.dvi
 cp /workdir/main.tex /workdir/handout.tex
 sed -i 's/]{beamer}/, handout]{beamer}/' /workdir/handout.tex
 
-rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log
+rm *.aux *.vrb *.nav *.out *.snm *.toc *.dvi *.log *.bbl *.blg
 uplatex handout.tex
 pbibtex handout
 uplatex handout.tex
